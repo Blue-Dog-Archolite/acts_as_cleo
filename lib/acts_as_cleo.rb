318e6ead01c513fd1d5506ead0b4f6ac39e78ba2
Robert R. Meyer <Blue.Dog.Archolite@gmail.com>
2012-04-11 17:03:24 -0500

    Fix for lib lib

--- lib/acts_as_cleo.rb
+++ lib/acts_as_cleo.rb
@@ -1,3 +1,3 @@
-require File.dirname(__FILE__) + '/cleo'
+require File.dirname(__FILE__) + '/cleo/cleo'
 require File.dirname(__FILE__) + '/acts_as_cleo/acts_as_cleo'
 #require File.dirname(__FILE__) + '/acts_as_cleo_connection/acts_as_cleo_connection'
